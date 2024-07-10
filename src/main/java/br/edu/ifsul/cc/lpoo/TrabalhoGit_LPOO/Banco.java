0061a944c702c2ec12e85ea92cf70d58a2343595
20221PF.CC0003 <20221PF.CC0003@pc-253373.passofundo.ifsul.local>
2024-07-10 10:39:00 -0300

    Dia 10/07

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/TrabalhoGit_LPOO/Banco.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.cc.lpoo.TrabalhoGit_LPOO;
+
+/**
+ *
+ * @author 20221PF.CC0003
+ */
+public class Banco {
+    
+    
+    
+}
